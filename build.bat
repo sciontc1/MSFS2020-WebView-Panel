957efb404369aed9c1d2f3bfde9a1d078d2d31f5
sciontc1 <george.rothon@googlemail.com>
2021-07-13 13:59:16 +0100

    Update to Build.bat
    Revised bat to simplify the process in getting files/folders in correct structure. This is to make it easier to simply copy to folder contained within "Build" to the required community location as a one time "copy and paste" action or further scripts may then use that to place the file as desired
    
    May further update with default community locations if present however I do not use these myself so hasn't been implemented.

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
 "%MSFS_SDK%\Tools\bin\fspackagetool.exe" "maximus-ingamepanels-custom\Build\maximus-ingamepanels-custom.xml" -nomirroring
 copy /Y "maximus-ingamepanels-custom\Build\Packages\maximus-ingamepanels-custom\Build\maximus-ingamepanels-custom.spb" "maximus-ingamepanels-custom\InGamePanels"
+RMDIR "Build\*.*" /S /Q
+TIMEOUT /T 3
+xcopy /e /v "maximus-ingamepanels-custom" "Build\maximus-ingamepanels-custom\" /y /s
+TIMEOUT /T 3
+RMDIR "Build\maximus-ingamepanels-custom\Build" /S /Q
